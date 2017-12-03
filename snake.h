e4e8181d73ac6f3d5c725a4b4579c7695485940a
theguynameddevin <31549347+theguynameddevin@users.noreply.github.com>
2017-12-02 17:54:00 -0800

    Update snake.h

--- snake.h
+++ snake.h
@@ -2,18 +2,29 @@
 #include "SFML/Window.hpp"
 #include "SFML/Graphics.hpp"
 
+using namespace sf;
 
-class Snake : public sf::RectangleShape
+class Snake : public sf::CircleShape
 {
+private:
+	Vector2f oldPosition;
 
 public:
 	
-	Snake(sf::Color const &newColor, sf::Vector2f const &size, sf::Vector2f const &pos)
+	Snake(sf::Color const &newColor, float radius, sf::Vector2f const &pos)
 	{
 		this->setFillColor(newColor);
-		this->setSize(size);
+		this->setRadius(radius);
 		this->setPosition(pos);
 	}
-};
 
+	void setOldPosition(Vector2f vector)
+	{
+		this->oldPosition = vector;
 
+	}
+	Vector2f getOldPosition()
+	{
+		return this->oldPosition;
+	}
+};  
