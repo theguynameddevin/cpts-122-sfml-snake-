1988ba81c169cb0ac65b4fd855b0c2b386f20f83
theguynameddevin <31549347+theguynameddevin@users.noreply.github.com>
2017-12-04 21:19:33 -0800

    Update maic.cpp

--- maic.cpp
+++ maic.cpp
@@ -13,25 +13,24 @@ using namespace std;
 
 int main()
 {
+	Font font;
+	font.loadFromFile("C:/Windows/Fonts/Arial.ttf");
 	srand((unsigned int)time(NULL));
 
 	RenderWindow window(VideoMode(600, 600),
 		"Hello SFML");
 
 	Color color = Color::Blue;
-	Text text;
-
-	text.setPosition(0, 0);
-	text.setString("Hello SFML");
+	
 	
 	vector<Snake> snake;
 	Snake newSnake(Color::Blue, window.getSize().x / 100,
 		Vector2f(window.getSize().x / 10, window.getSize().y / 10));
 	Dot newDot(Color::Blue, window.getSize().x / 100, Vector2f(12, 12));
-
+	newDot.setPosition(Vector2f((rand() % (window.getSize().x / 12)) * 12, (rand() % (window.getSize().y / 12)) * 12));
 	int index = 0;
 	int snakeSize = 1;
-	bool up = false, down = false, left = false, right = false;
+	bool up = false, down = false, left = false, right = false, game = false;
 	snake.push_back(newSnake);
 	newSnake.setPosition(snake[0].getPosition().x -( 2 * snake[0].getRadius()), snake[0].getPosition().y);
 	
@@ -41,172 +40,261 @@ int main()
 
 	while (window.isOpen())
 	{
-		window.draw(newDot);
-		window.draw(snake[0]);
 		Event event;
-		while (window.pollEvent(event))
+		/*while (window.pollEvent(event))
 		{
 			
+		
+			
+		}*/
+		Text menuText;
+		string Menu = "Snake\nCreated by: Michael Smith, Devin Hollander, Demetrius Anderson\n";
+		
+		Menu += "Press 1 to play\n Press 2 to view high scores \n press 3 to exit";
 
-			if (event.type == Event::Closed)
-				window.close();
-			//while ()
+		menuText.setString(Menu);
+
+		menuText.setFont(font);
+		menuText.setPosition(0, 0);
+		window.draw(menuText);
+		window.display();
+
+		while (window.pollEvent(event))
+		{
+			event.type == Event::KeyPressed;
+			while (event.key.code != Keyboard::Num1 && event.key.code != Keyboard::Num2 && event.key.code != Keyboard::Num3)
 			{
-				if (event.type == Event::KeyPressed)
+				if (event.key.code == Keyboard::Num1)
+				{
+					game = true;
+				}
+				if (event.key.code == Keyboard::Num2)
+				{
+
+				}
+				if (event.key.code == Keyboard::Num3)
+				{
+					window.close();
+					return 0;
+				}
+			}
+		}
+		window.clear();
+		snake.push_back(newSnake);
+		while (game == true)
+		{
+			window.draw(newDot);
+			window.draw(snake[0]);
+			Event event;
+			while (window.pollEvent(event))
+			{
+
+
+				if (event.type == Event::Closed)
+					window.close();
+				//while ()
+				{
+					if (event.type == Event::KeyPressed)
+					{
+						if (event.key.code == Keyboard::Up)	//up
+						{
+							up = true;
+							down = false;
+							left = false;
+							right = false;
+						}
+						else if (event.key.code == Keyboard::Down)	//down
+						{
+							down = true;
+							up = false;
+							left = false;
+							right = false;
+						}
+						else if (event.key.code == Keyboard::Left)	//left 
+						{
+							left = true;
+							up = false;
+							down = false;
+							right = false;
+						}
+						else if (event.key.code == Keyboard::Right)	//right
+						{
+							up = false;
+							down = false;
+							left = false;
+							right = true;
+						}
+					}
+				}
+
+				/*if (event.type == Event::KeyReleased)
 				{
 					if (event.key.code == Keyboard::Up)	//up
 					{
-						up = true;
-						down = false;
-						left = false;
-						right = false;
+						up = false;
 					}
 					else if (event.key.code == Keyboard::Down)	//down
 					{
-						down = true;
-						up = false;
-						left = false;
-						right = false;
-					}
-					else if (event.key.code == Keyboard::Left)	//left 
-					{
-						left = true;
-						up = false;
 						down = false;
-						right = false;
+					}
+					else if (event.key.code == Keyboard::Left)	//left
+					{
+						left = false;
 					}
 					else if (event.key.code == Keyboard::Right)	//right
 					{
-						up = false;
-						down = false;
-						left = false;
-						right = true;
+						right = false;
 					}
 				}
+				*/
+
+
+
+
 			}
 
-			/*if (event.type == Event::KeyReleased)
+			/*int prevX = snake[0].getPosition().x;
+			int prevY = snake[0].getPosition().y;*/
+			for (int i = 1; i < snakeSize; i++)
 			{
-				if (event.key.code == Keyboard::Up)	//up
+				if (snake[0].getPosition() == snake[i].getPosition() && snake[0].getPosition() != newDot.getPosition())
 				{
 					up = false;
-				}
-				else if (event.key.code == Keyboard::Down)	//down
-				{
 					down = false;
-				}
-				else if (event.key.code == Keyboard::Left)	//left 
-				{
 					left = false;
-				}
-				else if (event.key.code == Keyboard::Right)	//right
-				{
 					right = false;
+					game = false;
 				}
 			}
-			*/
 
+			if (index % 50 == 0)
+			{
+				for (int i = 0; i < snakeSize; i++)
+				{
+					snake[i].setOldPosition(snake[i].getPosition());
+				}
+				for (int i = snakeSize - 1; i > 0; i--)
+				{
+					snake[i].setPosition(snake[i - 1].getOldPosition());
+				}
+				if (up == true)	//up
+				{
+					//snake[1].setPosition(snake[0].getPosition());
+					snake[0].move(0, -2 * snake[0].getRadius());
 
+				}
+				if (down == true)	//down
+				{
+					//snake[1].setPosition(snake[0].getPosition());
+					snake[0].move(0, 2 * snake[0].getRadius());
+				}
+				if (left == true)	//left
+				{
+					//snake[1].setPosition(snake[0].getPosition());
+					snake[0].move(-2 * snake[0].getRadius(), 0);
+				}
+				if (right == true)	//right
+				{
+					//snake[1].setPosition(snake[0].getPosition());
+					snake[0].move(2 * snake[0].getRadius(), 0);
+				}
 
-			
-		}
+			}
+			/*
+			if (index % 50 == 0)
+			{
+				int prevX = snake[0].getPosition().x;
+				int prevY = snake[0].getPosition().y;
+				if (up == true)	//up
+				{
+					snake[0].move(0, -2 * snake[0].getRadius());
+					snake[1].setPosition(prevX, prevY);
+				}
+				if (down == true)	//down
+				{
+					snake[0].move(0, 2 * snake[0].getRadius());
+					snake[1].setPosition(prevX, prevY);
+				}
+				if (left == true)	//left
+				{
+					snake[0].move(-2 * snake[0].getRadius(), 0);
+					snake[1].setPosition(prevX, prevY);
+				}
+				if (right == true)	//right
+				{
+					snake[0].move(2 * snake[0].getRadius(), 0);
+					snake[1].setPosition(prevX, prevY);
+				}
+			}*/
+			index++;
+			window.clear();
 
-		/*int prevX = snake[0].getPosition().x;
-		int prevY = snake[0].getPosition().y;*/
-
-		
-		if (index % 50 == 0)
-		{
 			for (int i = 0; i < snakeSize; i++)
 			{
-				snake[i].setOldPosition(snake[i].getPosition());
+				window.draw(snake[i]);
 			}
-			for (int i = snakeSize - 1; i <0 ; i--)
+			//window.draw(snake[0]);
+			//window.draw(snake[1]);
+			window.draw(newDot);
+			
+			if (snake[0].getPosition().x < 0 || snake[0].getPosition().x > window.getSize().x - (2 * snake[0].getRadius()) ||
+				snake[0].getPosition().y < 0 || snake[0].getPosition().y > window.getSize().y - (2 * snake[0].getRadius()))
 			{
-				snake[i].setPosition(snake[i - 1].getOldPosition());
+				up = false;
+				down = false;
+				left = false;
+				right = false;
+				game = false;
 			}
-			if (up == true)	//up
+
+
+			if (snake[0].getPosition().x == newDot.getPosition().x && snake[0].getPosition().y == newDot.getPosition().y)
 			{
-				//snake[1].setPosition(snake[0].getPosition());
-				snake[0].move(0, -2 * snake[0].getRadius());
-				
-			}
-			if (down == true)	//down
-			{
-				//snake[1].setPosition(snake[0].getPosition());
-				snake[0].move(0, 2 * snake[0].getRadius());
-			}
-			if (left == true)	//left
-			{
-				//snake[1].setPosition(snake[0].getPosition());
-				snake[0].move(-2 * snake[0].getRadius(), 0);
-			}
-			if (right == true)	//right
-			{
-				//snake[1].setPosition(snake[0].getPosition());
-				snake[0].move(2 * snake[0].getRadius(), 0);
+
+				newDot.setPosition(Vector2f((rand() % (window.getSize().x / 12)) * 12, (rand() % (window.getSize().y / 12)) * 12));
+				newSnake.setPosition(snake[snakeSize - 1].getOldPosition());
+				snake.push_back(newSnake);
+				snakeSize++;
 			}
 			
+
+
+			window.display();
 		}
-		/*
-		if (index % 50 == 0)
-		{
-			int prevX = snake[0].getPosition().x;
-			int prevY = snake[0].getPosition().y;
-			if (up == true)	//up
-			{
-				snake[0].move(0, -2 * snake[0].getRadius());
-				snake[1].setPosition(prevX, prevY);
-			}
-			if (down == true)	//down
-			{
-				snake[0].move(0, 2 * snake[0].getRadius());
-				snake[1].setPosition(prevX, prevY);
-			}
-			if (left == true)	//left
-			{
-				snake[0].move(-2 * snake[0].getRadius(), 0);
-				snake[1].setPosition(prevX, prevY);
-			}
-			if (right == true)	//right
-			{
-				snake[0].move(2 * snake[0].getRadius(), 0);
-				snake[1].setPosition(prevX, prevY);
-			}
-		}*/
-		index++;
-		window.clear();
+	
+		
+		 
+		snake.clear();
+		
 
-		for (int i = 0; i < snakeSize; i++)
-		{
-			window.draw(snake[i]);
-		}
-		//window.draw(snake[0]);
-		//window.draw(snake[1]);
-		window.draw(newDot);
+		Text text;
+		text.setColor(Color::White);
+		text.setPosition(100, 100);
+		text.setFont(font);
 
-		if (snake[0].getPosition().x < 0 || snake[0].getPosition().x > window.getSize().x - (2*snake[0].getRadius()) ||
-			snake[0].getPosition().y < 0 || snake[0].getPosition().y > window.getSize().y - (2*snake[0].getRadius()))
-		{
-			up = false;
-			down = false; 
-			left = false;
-			right = false;
-		}
-
-		if (snake[0].getPosition().x == newDot.getPosition().x && snake[0].getPosition().y == newDot.getPosition().y)
-		{
-
-			newDot.setPosition(Vector2f((rand() % (window.getSize().x / 12)) * 12, (rand() % (window.getSize().y / 12)) * 12));
-			newSnake.setPosition(snake[snakeSize - 1].getOldPosition());
-			snake.push_back(newSnake);
-			snakeSize++;
-		}
-
+		string message = "Game over\nYour Score was ";
+		message += to_string(snakeSize);
+		snakeSize = 1;
+		text.setString(message);
+		window.draw(text);
 		window.display();
+		
+		while (window.pollEvent(event))
+		{
+			while (event.key.code != Keyboard::E)
+			{
+
+				if (event.key.code == Keyboard::E)
+				{
+					game = false;
+					window.clear();
+				}
+			}
+		}
 
 	}
 
 
 	return 0;
 }
+
